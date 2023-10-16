bcc76095f8e6299b54557bfcbe0bd484c08f9266
syamilardnsyh <syamilagni09@gmail.com>
2023-10-16 20:21:29 +0700

    first commit

---
+++ application/views/latihan1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Latihan</title>
+</head>
+<body>
+    <h1>Selamat Datang di Web Latihan</h1>
+</body>
+</html>
---
+++ application/views/view-latihan1.php
@@ -0,0 +1,9 @@
+<?php
+class Latihan1 extends CI_Controller 
+{
+    public function index()
+    {
+        echo "Selamat Datang.. selamat belajar web proggraming";
+        //$this->load->view('view-latihan1');
+    }
+}
