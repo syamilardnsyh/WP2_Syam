6e81806d7da49c4787849451221c3597e5e4e669
syamilardnsyh <syamilagni09@gmail.com>
2023-11-03 16:02:35 +0700

    TUGAS PROJECT WEB PROGGRAMING II

--- application/controllers/forgot_password.php
+++
@@ -1,5 +0,0 @@
-<?= form_open('forgot_password/send_reset_link'); ?>
-    <label for="email">Email</label>
-    <input type="email" name="email" required>
-    <button type="submit">Kirim Link Reset Password</button>
-<?= form_close(); ?>
--- application/views/view-latihan1.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Latihan1 extends CI_Controller 
-{
-    public function index()
-    {
-        echo "Selamat Datang.. selamat belajar web proggraming";
-        //$this->load->view('view-latihan1');
-    }
-}
