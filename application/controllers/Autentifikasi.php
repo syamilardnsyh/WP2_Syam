1de9f05bec1d2cc8157866f1bad27ad00f56a434
syamilardnsyh <syamilagni09@gmail.com>
2023-11-27 12:01:09 +0700

    Pertemuan XI

--- application/controllers/Autentifikasi.php
+++ application/controllers/Autentifikasi.php
@@ -3,9 +3,14 @@ class Autentifikasi extends CI_Controller
 {
     public function index()
     {
-        if ($this->session->userdata('role_id') == 2) {
+        // echo password_hash("123", PASSWORD_DEFAULT);die;
+
+        // Jika statusnya sudah login, maka tidak bisa mengakses halaman login 
+        // alias dikembalikan ke tampilan user
+        // session_destroy();die;
+        if ($this->session->userdata('role_id')==2) {
             redirect('user');
-        } elseif ($this->session->userdata('role_id') == 1) {
+        } elseif ($this->session->userdata('role_id')==1) {
             redirect('admin');
         }
 
@@ -21,6 +26,7 @@ class Autentifikasi extends CI_Controller
             $data['judul'] = 'Login';
             $data['user'] = '';
 
+            // Kata 'login' merupakan nilai dari variabel judul dalam array $data dikirimkan ke view aute_header
             $this->load->view('templates/aute_header', $data);
             $this->load->view('autentifikasi/login');
             $this->load->view('templates/aute_footer');
@@ -35,8 +41,11 @@ class Autentifikasi extends CI_Controller
         $password = $this->input->post('password', true);
         $user = $this->ModelUser->cekData(['email' => $email])->row_array();
 
+        // Jika usernya ada
         if ($user) {
+            // Jika user sudah aktif
             if ($user['is_active'] == 1) {
+                // Cek password
                 if (password_verify($password, $user['password'])) {
                     $data = [
                         'email' => $user['email'],
@@ -70,10 +79,61 @@ class Autentifikasi extends CI_Controller
     {
         $this->load->view('autentifikasi/blok');
     }
-
+    
     public function gagal()
     {
         $this->load->view('autentifikasi/gagal');
     }
+
+public function registrasi()
+{
+    // Jika pengguna sudah login, arahkan ke halaman user
+    if ($this->session->userdata('email')) {
+        redirect('user');
+    }
+
+    // Aturan untuk input nama: harus diisi
+    $this->form_validation->set_rules('nama', 'Nama Lengkap', 'required', [
+        'required' => 'Nama Belum diisi!!'
+    ]);
+
+    // Aturan untuk input email: harus diisi, format email yang valid, unik (belum terdaftar)
+    $this->form_validation->set_rules('email', 'Alamat Email', 'required|trim|valid_email|is_unique[user.email]', [
+        'valid_email' => 'Email Tidak Benar!!',
+        'required' => 'Email Belum diisi!!',
+        'is_unique' => 'Email Sudah Terdaftar!'
+    ]);
+
+    // Aturan untuk input password: harus diisi, minimal 3 karakter, dan harus sama dengan ulangi password
+    $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+        'matches' => 'Password Tidak Sama!!',
+        'min_length' => 'Password Terlalu Pendek'
+    ]);
+    $this->form_validation->set_rules('password2', 'Ulangi Password', 'required|trim|matches[password1]');
+
+    // Jika form belum disubmit atau validasi gagal, tampilkan form registrasi
+    if ($this->form_validation->run() == false) {
+        $data['judul'] = 'Registrasi Member';
+        $this->load->view('templates/aute_header', $data);
+        $this->load->view('autentifikasi/registrasi');
+        $this->load->view('templates/aute_footer');
+    } else {
+        // Jika validasi berhasil, simpan data pengguna ke dalam tabel user
+        $email = $this->input->post('email', true);
+        $data = [
+            'nama' => htmlspecialchars($this->input->post('nama', true)),
+            'email' => htmlspecialchars($email),
+            'image' => 'default.jpg',
+            'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+            'role_id' => 2,
+            'is_active' => 0,
+            'tanggal_input' => time()
+        ];
+        $this->ModelUser->simpanData($data); // menggunakan model
+        $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Selamat!! akun member anda sudah dibuat. Silahkan Aktivasi Akun anda</div>');
+        redirect('autentifikasi');
+    }
+}
+
 }
 ?>
